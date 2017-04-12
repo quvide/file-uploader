a25144902f9861e39ee53bfbf94055bb8864c6dd
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-04-13 00:14:57 +0300

    Use spaces and fix progress bar

--- nginx/static/index.js
+++ nginx/static/index.js
@@ -1,31 +1,32 @@
 function uploadFile(evt) {
-	evt.preventDefault();
-	var formData = new FormData($("#file")[0]);
+  evt.preventDefault();
+  var formData = new FormData($("#file")[0]);
   var xhr = new XMLHttpRequest();
-	xhr.addEventListener("progress", function(evt) {
-		if (evt.lengthComputable) {
-			$("#progess").attr({
-				value: evt.loaded,
-				max: evt.total
-			});
-		}
-	});
+  xhr.upload.addEventListener("progress", function(evt) {
+    if (evt.lengthComputable) {
+      console.log(evt.loaded, evt.total);
+      $("#progress").attr({
+        value: evt.loaded,
+        max: evt.total
+      });
+    }
+  });
 
-	xhr.addEventListener("load", function(evt) {
-		if (this.status == 200) {
-			var response = JSON.parse(this.responseText);
-			if (response.status == 0) {
-				window.location.href += response.uploaded_name;
-			} else if (response.status == 1) {
-				alert(response.error);
-			}
-		}
-	});
+  xhr.addEventListener("load", function(evt) {
+    if (this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      if (response.status == 0) {
+        window.location.href += response.uploaded_name;
+      } else if (response.status == 1) {
+        alert(response.error);
+      }
+    }
+  });
 
-	xhr.open("POST", "upload");
-	xhr.send(formData);
+  xhr.open("POST", "upload");
+  xhr.send(formData);
 }
 
 $(function() {
-	$("#file").submit(uploadFile);
+  $("#file").submit(uploadFile);
 })
